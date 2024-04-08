3f6370de4b2bc363101acf78ecf07925dfa8c966
Tabish Zaidi <164076772+TabishZaidi1@users.noreply.github.com>
2024-04-09 00:44:38 +0500

    push my currency converter machine

---
+++ index.ts
@@ -0,0 +1,56 @@
+#!/usr/bin/env node
+import inquirer from "inquirer"
+
+
+ const currency:any = {
+
+USD: 1,
+PKR: 278,
+EUR: 0.92,
+lIRA: 32.02,
+IND: 83.29
+
+
+};
+
+ let user_answer = await inquirer.prompt(
+       [
+              {
+  
+       name: "from",
+       message: "Enter from Currency",
+       type: "list",
+       choices: ["USD", "PKR", "EUR", "LIRA", "IND"]
+
+              },
+       
+              {
+  
+                     name: "to",
+                     message: "Enter to Currency",
+                     type: "list",
+                     choices: ["USD", "PKR", "EUR", "LIRA", "IND"]
+
+               },
+
+               {
+  
+                     name: "amount",
+                     message: "Enter your amount",
+                     type: "number"
+                     
+              
+              },
+
+       ]
+
+)
+
+let fromAmount = currency[user_answer.from];
+let toAmount = currency[user_answer.to];
+let amount = user_answer.amount
+let baseAmount = amount / fromAmount
+let convertedAmount = baseAmount * toAmount
+
+console.log(convertedAmount); 
+
